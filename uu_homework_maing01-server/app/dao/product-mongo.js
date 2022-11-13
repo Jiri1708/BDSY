a31bc3d7bb11f6c14be3e1629688cee25724293f
Jiri Teplicky <jiri.teplicky@unicornuniversity.net>
2022-11-13 10:29:13 +0100

    dao created

---
+++ uu_homework_maing01-server/app/dao/product-mongo.js
@@ -0,0 +1,36 @@
+"use strict";
+const { UuObjectDao } = require("uu_appg01_server").ObjectStore;
+
+class ProductMongo extends UuObjectDao {
+  async createSchema() {
+    await super.createIndex({ awid: 1 }, { unique: true });
+  }
+
+  async create(uuObject) {
+    return await super.insertOne(uuObject);
+  }
+  async get(awid, id) {
+    let filter = {
+      awid: awid,
+      id: id,
+    };
+
+    return await super.findOne(filter);
+  }
+  async remove(uuObject) {
+    let filter = {
+      awid: awid,
+      id: id,
+    };
+    return await super.deleteOne(filter);
+  }
+  async update(uuObject) {
+    let filter = {
+      awid: awid,
+      id: id,
+    };
+    return await super.findOneAndUpdate(filter, uuObject, "NONE");
+  }
+}
+
+module.exports = ProductMongo;
